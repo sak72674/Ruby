f917a64e8ffedb347e4c784c1fcaf36e249d2942
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-07-01 15:37:55 +0900

    Start 2-6

---
+++ number.rb
@@ -0,0 +1,12 @@
+puts 150 + 200
+
+name = "saori"
+puts "Hello " + name
+puts "How are you #{name}?"
+
+names = ["Peal", "Ruby", "Python", "Swift"]
+puts names[1]
+
+tall = {"太郎"=>185, "次郎"=>170, "花子"=>150}
+puts tall["太郎"]
+
