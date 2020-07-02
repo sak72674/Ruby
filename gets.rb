0c564609d32da5c80731a8acf3a1a04d75798a90
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-07-02 18:04:37 +0900

    Start 2-8

---
+++ gets.rb
@@ -0,0 +1,31 @@
+# puts "あいうえお"
+# input_kye = gets
+# puts "鍵は"+input_kye
+
+# puts "キーボードで数字「2」と数字「3」を入力してください"
+# a=gets.to_i
+# b=gets.to_i
+# puts "a+b=#{a+b}"
+
+# dice = 0
+# while dice != 6 do
+# 	dice = rand(1..6)
+# 	puts dice
+# end
+
+# for i in 1..10 do
+# 	puts i
+# end
+
+# {"orange"=>12, "apple"=>5, "pear"=>7}.each do |fruits, count|
+# 	puts "#{fruits}は#{count}個です。"
+# end
+
+i = 0
+while i <= 10 do
+  if i >5
+    break
+  end
+  puts i
+  i += 1
+end
